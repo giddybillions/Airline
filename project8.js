3739217dd7759190a3776d4394015399a79dafa6
giddybillions <gideonagbedogun@gmail.com>
2025-10-05 13:10:22 -0700

    first commit

--- project8.js
+++ project8.js
@@ -5,21 +5,16 @@ function sendMail(){
         message: document.getElementById("message").value ,
     };
 
-
 const serviceID = "service_be2tcx8";
 const  templateID ="template_s5gynu1"
 
-emailjs
- .send(serviceID,templateID, params)
- .then(
-    res =>{
+emailjs.send(serviceID,templateID, params).then(res =>{
         document.getElementById("name").value = "",
         document.getElementById("email").value = "",
-        document.getElementById("message").value = "",
+        document.getElementById("message").value = "";
 
         console.log(res);
         alert("your message sent successfully")
-    }
-)
-.catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
 }
